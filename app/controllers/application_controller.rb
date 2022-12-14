76570aa1b73bddc3add78fb48d7fb993b9f406e5
masato-kobayashi5427 <savracoco@gmail.com>
2022-12-14 14:05:46 +0900

    バリデーションの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
+  before_action :authenticate_user!, only: :new
 
-  class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-  end
-  
   private
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day])
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
-
+  validates :image, presence: true
   validates :name, presence: true
   validates :explain, presence: true
+  NUM_HANKAKU = /\A[0-9]+\z/.freeze
+  validates_format_of :price, with: NUM_HANKAKU, message: 'は半角数字で入力してください'
   validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
@@ -11,12 +15,11 @@ class Product < ApplicationRecord
   validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :user, presence: true
 
-  belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id
-  belongs_to :prefecture_id
-  belongs_to :state_id
-  belongs_to :fee_id
-  belongs_to :delivery_time_id
+  belongs_to :user
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :state
+  belongs_to :fee
+  belongs_to :delivery_time
 end
