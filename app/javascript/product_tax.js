7cd8d91aa85713790c54a7f9d664513c6c423fde
masato-kobayashi5427 <savracoco@gmail.com>
2022-12-15 12:02:20 +0900

    販売手数料のjavascriptを実装

---
+++ app/javascript/product_tax.js
@@ -0,0 +1,15 @@
+function product_tax (){
+  const productPrice = document.getElementById("item-price");
+  productPrice.addEventListener("keyup", () => {
+    const commission = productPrice.value * 0.1;
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const commissionInt = Math.trunc(commission);
+    addTaxPrice.innerHTML = `${commissionInt}`;
+    const salesProfit = productPrice.value - commission ;
+    const profit = document.getElementById("profit");
+    const salesProfitInt = Math.trunc(salesProfit);
+    profit.innerHTML = `${salesProfitInt}`;
+  });
+}
+
+window.addEventListener('load', product_tax);
