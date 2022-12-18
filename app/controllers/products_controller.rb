89dd0fb678fcd5f7d0387695d1478d9910b58781
masato-kobayashi5427 <savracoco@gmail.com>
2022-12-18 17:24:10 +0900

    商品編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, except: [:index, :show]
   
   def index
     @products = Product.order("created_at DESC")
@@ -22,10 +23,31 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to "/products/#{@product.id}"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :explain, :price, :category_id, :prefecture_id, :state_id, :fee_id, :delivery_time_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    if user_signed_in? && current_user.id == @product.user_id
+    else
+      redirect_to action: :index
+    end
   end
 end
 
